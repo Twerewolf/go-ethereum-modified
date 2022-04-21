67e2564540c3eaec8ca91355b30d331fa72deb1a
Twerewolf <405146266@qq.com>
2022-04-21 16:08:24 +0800

    log_monitor

--- tjw/privatechain/log_monitor.sh
+++ tjw/privatechain/log_monitor.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-LOGPATH=/home/tjw/CodeRoot/go-ethereum/tjw/privatechain
+LOGPATH=../../tjw/privatechain
 
-tail -f $LOGPATH/gethConsole.log #-f 循环读取，用于查看递增的日志文件
+#-f 循环读取，用于查看递增的日志文件
+tail -f $LOGPATH/gethConsole.log 
